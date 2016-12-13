8829e2935bbac6a0d4b2270ca59d17f1f1331fd4
Etrama <kaushikmoudgalya@gmail.com>
2016-12-13 12:30:13 +0530

    consolidated

--- demo.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import scipy
-
-
-print "np : " + np.__version__
-print "scp : " + scipy.__version__
--- trial1_issue1fix.py
+++
@@ -1,5 +0,0 @@
-input = raw_input("Save images? (y/n)")
-if input == "y":
-
-else:
-     
