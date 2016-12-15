327b968b91a09a8afa74365c0299fdc149f64162
Etrama <kaushikmoudgalya@gmail.com>
2016-12-15 18:58:07 +0530

    Added collage feature'

--- 9gag_scraper.py
+++ 9gag_scraper.py
@@ -54,3 +54,50 @@ if ui == 'y' or ui == "Y":
 	print "Images saved."
 else:
 	print "Images not saved."
+
+ui2 = raw_input("Make collage of downloaded images? (y/n)")
+
+if (ui == 'y' or ui == 'Y') and (ui2 =='y' or ui2 == 'Y'):
+    #collage code goes here
+    import PIL
+    sizes = []
+	for i in range(len(imgalts)):
+		im = Image.open(dirloc+"/"imgalts[i]+".jpeg")
+		sizes.append(im.size)
+
+	heights = []
+	widths = []
+	for j in range(len(sizes)):
+		heights.append(sizes[j][1])
+		widths.append(sizes[j][0])
+
+	resizedims = []
+	for i in range(len(heights)):
+		im = Image.open(dirloc+"/"+imgalts[i]+".jpeg")
+		resized.append(im.resize((sizes[i][0],min(heights)),PIL.Image.ANTIALIAS))
+
+	new_im = Image.new('RGB',(2*max(widths),5*min(heights)))
+	i = 0
+	x = 0
+	y = 0
+
+	for cols in range(2):
+		for rows in range(5):
+			print(i,x,y)
+			new_im.paste(resizedims[i],(x,y))
+			i = i + 1
+			y = y + min(heights)
+		x = x + max(widths)
+		y = 0
+
+	ui3 = raw_input("Enter location to save collage: ")
+	new_im.save(ui3+"Collage.jpeg")
+	im = Image.open(ui3+"Collage.jpeg")
+	im.show()
+
+else:
+    print("If images weren't saved, collage cannot be constructed.")
+    print("Or, you chose not to make a collage.")   
+
+  
+    
